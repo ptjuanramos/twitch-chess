ba7ad3d482696564e984d4e2853f54d7992b4685
Juan Ramos <ptjuanramos@gmail.com>
2021-09-27 22:08:21 +0100

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+// Lines 5-8: Bring in chessboard and chess.js stuff
+import Chessboard from "chessboardjsx";
+const Chess = require("chess.js");
 
-function App() {
+
+const App = () => {
+
+  const [nextMovePlayer, setNextMovePlayer] = useState('b');
+
+  const [gameIsOver, setGameIsOver] = useState(false);
+  
+  const [chess] = useState(new Chess());
+  const [fen, setFen] = useState(chess.fen());
+
+  const handleMove = (move) => {
+    if (chess.move(move)) {
+      setFen(chess.fen());
+    }
+
+    if (chess.game_over()) {
+      handleGameOver();
+    }
+
+  };
+
+  const handleGameOver = () => {
+    if (chess.game_over()) {
+      setGameIsOver(true);
+      setNextMovePlayer(chess.turn());
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex-center">
+      <Chessboard
+        width={400}
+        position={fen}
+        onDrop={(move) =>
+          handleMove({
+            from: move.sourceSquare,
+            to: move.targetSquare,
+            promotion: "q",
+          })
+        }
+      />
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <b>Game is Over</b>
+        </div>
+
+        <div class="panel-body">
+          Panel content
+        </div>
+      </div>
     </div>
   );
 }
